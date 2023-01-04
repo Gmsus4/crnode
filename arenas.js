2f9113aa89431fe6bd86c172a7af16f63f63096b
fer <fernando452000@gmail.com>
2023-01-04 10:15:34 -0600

    He agregado una base de datos con la url de imágenes de las arenas para poderlas implementar en la página

---
+++ arenas.js
@@ -0,0 +1,82 @@
+module.exports = [
+    {
+        "name": "Arena 20",
+        "url": "https://www.deckshop.pro/img/arena/arena_champion.png"
+    },
+    {
+        "name": "Arena 19",
+        "url": "https://www.deckshop.pro/img/arena/arena_dragon.png"
+    },
+    {
+        "name": "Arena 18",
+        "url": 'https://www.deckshop.pro/img/arena/arena_sanctuary.png'
+    },
+    {
+        "name": "Arena 17",
+        "url": 'https://www.deckshop.pro/img/arena/arena_tomb.png'
+    },
+    {
+        "name": "Arena 16",
+        "url": 'https://www.deckshop.pro/img/arena/arena_kitchen.png'
+    },
+    {
+        "name": "Arena 15",
+        "url": "https://www.deckshop.pro/img/arena/arena_mine.png"
+    },
+    {
+        "name": "Arena 14",
+        "url": "https://www.deckshop.pro/img/arena/arena_lunar.png"
+    },
+    {
+        "name": "Arena 13",
+        "url": "https://www.deckshop.pro/img/arena/arena_tree.png"
+    },
+    {
+        "name": "Arena 12",
+        "url": "https://www.deckshop.pro/img/arena/arena_spooky.png"
+    },
+    {
+        "name": "Arena 11",
+        "url": "https://www.deckshop.pro/img/arena/arena_electro.png"
+    },
+    {
+        "name": "Arena 10",
+        "url": "https://www.deckshop.pro/img/arena/arena_hogmountain.png"
+    },
+    {
+        "name": "Arena 9",
+        "url": "https://www.deckshop.pro/img/arena/arena_jungle.png"
+    },
+    {
+        "name": "Arena 8",
+        "url": "https://www.deckshop.pro/img/arena/arena_ice.png"
+    },
+    {
+        "name": "Arena 7",
+        "url": "https://www.deckshop.pro/img/arena/arena_royal.png"
+    },
+    {
+        "name": "Arena 6",
+        "url": "https://www.deckshop.pro/img/arena/arena_pekka.png"
+    },
+    {
+        "name": "Arena 5",
+        "url": "https://www.deckshop.pro/img/arena/arena_builders.png"
+    },
+    {
+        "name": "Arena 4",
+        "url": "https://www.deckshop.pro/img/arena/arena_spell.png"
+    },
+    {
+        "name": "Arena 3",
+        "url": "https://www.deckshop.pro/img/arena/arena_barbarian.png"
+    },
+    {
+        "name": "Arena 2",
+        "url": "https://www.deckshop.pro/img/arena/arena_bone.png"
+    },
+    {
+        "name": "Arena 1",
+        "url": "https://www.deckshop.pro/img/arena/arena_goblin.png"
+    }
+];
