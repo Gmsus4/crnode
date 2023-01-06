1e39a905b9c0de978ff4facc5cd148c75cedc874
fer <fernando452000@gmail.com>
2023-01-06 14:36:53 -0600

    Se modificó la página de búsqueda, ahora es muy llamativa visualmente por el css y una imágen que agregué, pero falta agregar más estilos para que sea responsive

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ const chests = require('./data/chests');
 const arenas = require('./data/arenas');
 const exp = require('./data/exp');
 
-const hostClan = 'http://localhost:3000/clan?id=';
-const hostUser = 'http://localhost:3000/search?playerId='
+const hostClanOld = 'http://localhost:3000/clan?id=';
+const hostUserOld = 'http://localhost:3000/search?playerId=';
+const hostUser = 'http://localhost:3000/search?searchType=player&query='
+const hostClan = 'http://localhost:3000/search?searchType=clanById&query=';
 
 app.use(express.static('public'));
 
@@ -63,158 +65,240 @@ app.get('/', (req, res) =>{
     res.render('index');
 })
 
-app.get('/search', (req, res) => {
-  const playerId = req.query.playerId;
+app.get("/search", function(req, res) {
+  // req.query = { searchType: 'clan', query: 'Perro' }
+  const searchType = req.query.searchType; //El método a utilizar, sea buscar por clan id, clan o id de usuario
+  const query = req.query.query; //El dato que se escribió en el buscador
+  /* console.log(searchType) */
 
-  request(`https://api.clashroyale.com/v1/players/%23${playerId}`, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`
-    }
-  }, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else {
-      let urlHero = [];
-      let urlArena = [];
-      let urlCurrentDeck = [];
-      const datosJugador = JSON.parse(body);
-      const currentFavouriteCard = datosJugador.currentFavouriteCard.name;
-      const currentDeck = datosJugador.currentDeck;
-      const clanId = datosJugador.clan.tag.slice(1);
-
-      /* console.log(clanId); */
-
-      for(let arena in arenas){
-        if(arenas[arena].name === datosJugador.arena.name){
-/*           console.log(arenas[arena].url); */
-          urlArena.push(arenas[arena].url);
-        }
+  if (searchType === "player") {
+    const playerId = req.query.query;
+    request(`https://api.clashroyale.com/v1/players/%23${playerId}`, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`
       }
-      
-      // Aquí iría tu lógica para procesar los datos del jugador
-      for(let seed in seeds){ //Itera el formato JSON el seed será el valor iterado, es decir, cuantas veces se iteró
-        if(seeds[seed].name === currentFavouriteCard){
-          urlHero =  `https://cdn.statsroyale.com/images/characters/${seeds[seed].key}.jpg`;
-              for (let i = 0; i < seeds.length; i++) {
-                for (let j = 0; j < currentDeck.length; j++) {
-                    if (seeds[i].name === currentDeck[j].name) {
-                        urlCurrentDeck.push(`https://cdn.statsroyale.com/images/cards/full/${seeds[i].key}.png`);
-                      }
-                }
-              }
-        }
-      }
-
-      let dataUrl = {
-        imgCards: urlCurrentDeck, 
-        imgBanner: urlHero,
-        imgArena: urlArena
-      }
-
-      /* EXP */
-      let maxXp = 0; // Nivel máximo de XP
-
-/*       Comparación de xp para poder saber el la experiencia máxima dependiendo su nivel */
-      const userLevel = datosJugador.expLevel; //Nivel del jugador
-      for(let e in exp){ //Iteración para coincidir con el mismo nivel
-        if(userLevel == exp[e].name){
-            maxXp = exp[e].exp_to_next_level; //Cambiando el valor máximo del xp
-        }
-      }
-
-      const currentXp = datosJugador.expPoints; // XP actual
-    
-      const widthProgress = ((currentXp / maxXp) * 100).toFixed(2) + '%';
-
-      const dataExp = {
-          width: widthProgress,
-          user: userLevel,
-          max: maxXp
-      }
-
-      /* Cofres */      
-      request(`https://api.clashroyale.com/v1/players/%23${playerId}/upcomingchests`, {
-        headers: {
-          Authorization: `Bearer ${API_KEY}`
-        }
-      }, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          const urlChest = [];
-          const upcomingChests = JSON.parse(body);
-
-          for (let i = 0; i < chests.length; i++) {
-            for (let j = 0; j < upcomingChests.items.length; j++) {
-                if (chests[i].name === upcomingChests.items[j].name) {
-/*                     urlChest.push(chests[i].url); */
-                    urlChest.push({url: chests[i].url, index: upcomingChests.items[j].index});
-                  }
-            }
+    }, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        let urlHero = [];
+        let urlArena = [];
+        let urlCurrentDeck = [];
+        const datosJugador = JSON.parse(body);
+        const currentFavouriteCard = datosJugador.currentFavouriteCard.name;
+        const currentDeck = datosJugador.currentDeck;
+        const clanId = datosJugador.clan.tag.slice(1);
+  
+        /* console.log(clanId); */
+  
+        for(let arena in arenas){
+          if(arenas[arena].name === datosJugador.arena.name){
+  /*           console.log(arenas[arena].url); */
+            urlArena.push(arenas[arena].url);
           }
-
-          let filterArray = urlChest.filter(item => typeof item === 'object' && //Filtrar el array para mantener los objetos con propiedad "index"
-          item.hasOwnProperty('index'));
-          let sortedArray = filterArray.sort((a, b) => a.index - b.index);
-          let chestUrlSorted = sortedArray.map(item => item.url);
-
-          request(`https://api.clashroyale.com/v1/clans/%23${clanId}`,{
-            headers: {
-              Authorization: `Bearer ${API_KEY}`
-            }
-          }, (error, response, body) => {
-            if (error) {
-              console.error(error);
-            } else {
-                const datosClan = JSON.parse(body);
-                const idClan = datosClan.memberList;
-                const lastTime = [];
-
-                for(let id in idClan){
-                  if(idClan[id].tag === datosJugador.tag){
-                    let str = idClan[id].lastSeen; //El valor será cada un (Última conexión) por cada uno
-                    const arr = []; //Todo esto es para modivicar el valor de la última conexión para que sea válido
-                    const pattern = [4, 2, 5, 2, 2, 5];
-            
-                    let i = 0;
-                    for (const size of pattern) {
-                      arr.push(str.slice(i, i + size));
-                      i += size;
-                    }
-                    str = (`${arr[0]}-${arr[1]}-${arr[2]}:${arr[3]}:${arr[4]}${arr[5]}`);
-                    /* console.log(str); */
-
-                    //Ojo, que volvimos a repetir código. Pero no encontré otra manera de simplificar el código, ya será en otra ocasión
-                    let lastSeen = new Date(str);
-                    let currentTime = new Date();
-          
-                    let milisegundo = (currentTime.getTime() - lastSeen.getTime());
-                    let segundo = (currentTime.getTime() - lastSeen.getTime()) / 1000;
-                    let minuto = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60);
-                    let hora = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60); 
-                    let day = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24); 
-                    let week = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24 * 7); 
-                    let month = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24 * 30.4167); 
-                    let year = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24 * 7 * 52); 
-                    getTimeDate(milisegundo, segundo, minuto, hora, day, week, month, year, lastTime);
+        }
+        
+        // Aquí iría tu lógica para procesar los datos del jugador
+        for(let seed in seeds){ //Itera el formato JSON el seed será el valor iterado, es decir, cuantas veces se iteró
+          if(seeds[seed].name === currentFavouriteCard){
+            urlHero =  `https://cdn.statsroyale.com/images/characters/${seeds[seed].key}.jpg`;
+                for (let i = 0; i < seeds.length; i++) {
+                  for (let j = 0; j < currentDeck.length; j++) {
+                      if (seeds[i].name === currentDeck[j].name) {
+                          urlCurrentDeck.push(`https://cdn.statsroyale.com/images/cards/full/${seeds[i].key}.png`);
+                        }
                   }
                 }
-                /* console.log(lastTime[0]); */
-                // Aquí se envía la respuesta con los datos del jugador y los cofres por venir
-                res.render('data.ejs',{
-                  cofresPorVenir: {chestUrlSorted, upcomingChests},
-                  url: dataUrl,
-                  player: datosJugador,
-                  exp: { dataExp },
-                  hostClan, 
-                  lastTime
-                });
-            }
-          });
+          }
         }
-      });
-    }
-  });
+  
+        let dataUrl = {
+          imgCards: urlCurrentDeck, 
+          imgBanner: urlHero,
+          imgArena: urlArena
+        }
+  
+        /* EXP */
+        let maxXp = 0; // Nivel máximo de XP
+  
+  /*       Comparación de xp para poder saber el la experiencia máxima dependiendo su nivel */
+        const userLevel = datosJugador.expLevel; //Nivel del jugador
+        for(let e in exp){ //Iteración para coincidir con el mismo nivel
+          if(userLevel == exp[e].name){
+              maxXp = exp[e].exp_to_next_level; //Cambiando el valor máximo del xp
+          }
+        }
+  
+        const currentXp = datosJugador.expPoints; // XP actual
+      
+        const widthProgress = ((currentXp / maxXp) * 100).toFixed(2) + '%';
+  
+        const dataExp = {
+            width: widthProgress,
+            user: userLevel,
+            max: maxXp
+        }
+  
+        /* Cofres */      
+        request(`https://api.clashroyale.com/v1/players/%23${playerId}/upcomingchests`, {
+          headers: {
+            Authorization: `Bearer ${API_KEY}`
+          }
+        }, (error, response, body) => {
+          if (error) {
+            console.error(error);
+          } else {
+            const urlChest = [];
+            const upcomingChests = JSON.parse(body);
+  
+            for (let i = 0; i < chests.length; i++) {
+              for (let j = 0; j < upcomingChests.items.length; j++) {
+                  if (chests[i].name === upcomingChests.items[j].name) {
+  /*                     urlChest.push(chests[i].url); */
+                      urlChest.push({url: chests[i].url, index: upcomingChests.items[j].index});
+                    }
+              }
+            }
+  
+            let filterArray = urlChest.filter(item => typeof item === 'object' && //Filtrar el array para mantener los objetos con propiedad "index"
+            item.hasOwnProperty('index'));
+            let sortedArray = filterArray.sort((a, b) => a.index - b.index);
+            let chestUrlSorted = sortedArray.map(item => item.url);
+  
+            request(`https://api.clashroyale.com/v1/clans/%23${clanId}`,{
+              headers: {
+                Authorization: `Bearer ${API_KEY}`
+              }
+            }, (error, response, body) => {
+              if (error) {
+                console.error(error);
+              } else {
+                  const datosClan = JSON.parse(body);
+                  const idClan = datosClan.memberList;
+                  const lastTime = [];
+  
+                  for(let id in idClan){
+                    if(idClan[id].tag === datosJugador.tag){
+                      let str = idClan[id].lastSeen; //El valor será cada un (Última conexión) por cada uno
+                      const arr = []; //Todo esto es para modivicar el valor de la última conexión para que sea válido
+                      const pattern = [4, 2, 5, 2, 2, 5];
+              
+                      let i = 0;
+                      for (const size of pattern) {
+                        arr.push(str.slice(i, i + size));
+                        i += size;
+                      }
+                      str = (`${arr[0]}-${arr[1]}-${arr[2]}:${arr[3]}:${arr[4]}${arr[5]}`);
+                      /* console.log(str); */
+  
+                      //Ojo, que volvimos a repetir código. Pero no encontré otra manera de simplificar el código, ya será en otra ocasión
+                      let lastSeen = new Date(str);
+                      let currentTime = new Date();
+            
+                      let milisegundo = (currentTime.getTime() - lastSeen.getTime());
+                      let segundo = (currentTime.getTime() - lastSeen.getTime()) / 1000;
+                      let minuto = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60);
+                      let hora = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60); 
+                      let day = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24); 
+                      let week = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24 * 7); 
+                      let month = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24 * 30.4167); 
+                      let year = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24 * 7 * 52); 
+                      getTimeDate(milisegundo, segundo, minuto, hora, day, week, month, year, lastTime);
+                    }
+                  }
+                  /* console.log(lastTime[0]); */
+                  // Aquí se envía la respuesta con los datos del jugador y los cofres por venir
+                  res.render('player.ejs',{
+                    cofresPorVenir: {chestUrlSorted, upcomingChests},
+                    url: dataUrl,
+                    player: datosJugador,
+                    exp: { dataExp },
+                    hostClan, 
+                    lastTime
+                  });
+              }
+            });
+          }
+        });
+      }
+    });
+  } else if (searchType === "clan") {
+    res.render('pruebaClan.ejs', {query});
+  } else if (searchType === "clanById") {
+    const clanId = req.query.query;
+    request(`https://api.clashroyale.com/v1/clans/%23${clanId}`, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`
+      }
+    }, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        const datosClan = JSON.parse(body);
+        const idClanBadges = datosClan.badgeId;
+       /*  console.log(idClanBadges); */
+        let rgb;
+        
+        getImageColors(`https://cdn.statsroyale.com/images/badges/${idClanBadges}.png`).then(colors => {
+          primaryColor = colors[0];
+          const r = primaryColor._rgb[0];
+          const g = primaryColor._rgb[1];
+          const b = primaryColor._rgb[2];
+        
+          rgb = {
+            red: r, 
+            green: g,
+            blue: b
+          }
+  
+  
+          const lastSeenClan = datosClan.memberList;
+          const listMod = []; //Lista de la última conexión de los jugadores. Sin validacion en el formato
+          const listOk = []; //Lista de la última conexión de los jugadores pero la que tiene validación
+          let times = []; //Lista original con el tiempo en vivo sobre la última conexión de los jugadores
+  
+          for(let last in lastSeenClan){ //Iteración de la última conexión de todos los jugadores
+            listMod.push(lastSeenClan[last].lastSeen); //Agregamos a la lista la última conexión de todos los jugadores (Sin validacion en el formato)
+          }
+  
+          for(let list in listMod){ //Iteramos esa lista nueva que creamos
+            const str = listMod[list] //El valor será cada un (Última conexión) por cada uno
+            const arr = []; //Todo esto es para modivicar el valor de la última conexión para que sea válido
+            const pattern = [4, 2, 5, 2, 2, 5];
+    
+            let i = 0;
+            for (const size of pattern) {
+              arr.push(str.slice(i, i + size));
+              i += size;
+            }
+            /* console.log(`${arr[0]}-${arr[1]}-${arr[2]}:${arr[3]}:${arr[4]}${arr[5]}`); */
+            listOk.push(`${arr[0]}-${arr[1]}-${arr[2]}:${arr[3]}:${arr[4]}${arr[5]}`); // Finalmente lo hacemos válido y hacemos push para agregarlo al array. Con esto todos tienen un valor único de cada usuario de su última conexión
+          }
+  
+          for(let list in listOk){ //Esta iteración, es para iterar cada uno de los usuarios con el date valido
+            let lastSeen = new Date(listOk[list]); //Aquí lo transformamos, recuerda, a cada uno de ellos
+            let currentTime = new Date(); //Obtenemos la fecha y la hora exacta actualmente para poder comparar el tiempo de la última conexión
+  
+            //Sacamos la diferencia entre el tiempo en milisegundos, la cual podemos comparar con el tiempo actual y así sacar una diferencia
+            //Entre milisegundos, segundos, minutos, horas, días, semanas, meses o hasta años
+            let milisegundo = (currentTime.getTime() - lastSeen.getTime()); // diferencia en milisegundos
+            let segundo = (currentTime.getTime() - lastSeen.getTime()) / 1000; // diferencia en segundos
+            let minuto = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60); //Diferencia en minutos
+            let hora = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60); //Diferencia en horas
+            let day = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24); //Diferencia en días
+            let week = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24 * 7); //Diferencia en semanas
+            let month = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24 * 30.4167); //Diferencia en meses
+            let year = (currentTime.getTime() - lastSeen.getTime()) / (1000 * 60 * 60 * 24 * 7 * 52); //Diferencia en años
+            getTimeDate(milisegundo, segundo, minuto, hora, day, week, month, year, times);
+          }
+          res.render('clan.ejs', { clan: {datosClan}, color: rgb, hostUser, times });
+  /*         res.send({ clan: {datosClan}, color: rgb }); */
+        });
+      }
+    });
+  } else {
+      res.send("Tipo de búsqueda no válido");
+  }
 });
 
 app.get('/clan', (req, res) =>{
@@ -289,8 +373,8 @@ app.get('/clan', (req, res) =>{
       });
     }
   });
-  
-})
+});
+
 app.listen(3000, () => {
   console.log('Servidor iniciado en el puerto 3000');
 });
